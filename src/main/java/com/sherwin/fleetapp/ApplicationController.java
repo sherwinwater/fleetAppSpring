60d9f0e9e914d8e21ab8d608d719f40be89d1b50
sherwin <sherwin@liMac.local>
2020-07-10 17:10:40 -0400

    first commit

--- src/main/java/com/sherwin/fleetapp/ApplicationController.java
+++ src/main/java/com/sherwin/fleetapp/ApplicationController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ApplicationController {
 
-    @GetMapping("/index")
+    @GetMapping("/")
     public String goHome(){
         return "index";
     }
